b7425c335fb07607bb380ddf4126d99550a9b970
Nnvedward <nnvedward@gmail.com>
2022-01-25 01:02:35 +0100

    Displaying the expense items to the browser using react-redux

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
 import AppRouter from './routers/AppRouter'
@@ -12,13 +13,21 @@ const store = configureStore()
 
 store.dispatch(addExpense({ description: 'Water bill', amount: '200' }))
 store.dispatch(addExpense({ description: 'Gas bill', amount: '100' }))
-
 store.dispatch(setTextFilter('water'))
+
+setTimeout(() => {
+    store.dispatch(setTextFilter('bill'))
+}, 4000)
 
 
 const state = store.getState()
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
 console.log(visibleExpenses)
 
+const jsx = (
+    <Provider store={store}>
+        <AppRouter />
+    </Provider>
+)
 
-ReactDOM.render(<AppRouter />, document.getElementById('app'))
+ReactDOM.render(jsx, document.getElementById('app'))
