003d0a082ea5660d0d51415a0a0fc7ec63c1e35d
Nnvedward <nnvedward@gmail.com>
2022-01-26 10:28:56 +0100

    Wiring up add expense

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -1,11 +1,21 @@
 import React from "react"
+import { connect } from "react-redux"
 import ExpenseForm from "./ExpenseForm"
+import { addExpense } from "../actions/expenses"
+import { useNavigate } from "react-router-dom"
 
-const AddExpensePage = () => (
-    <div>
-        <h1>Add Expense</h1>
-        <ExpenseForm />
-    </div>
-)
+const AddExpensePage = (props) => {
+    const history = useNavigate()
+    return (
+        <div>
+            <h1>Add Expense</h1>
+            <ExpenseForm
+                handleSubmit={(expense) => {
+                    props.dispatch(addExpense(expense))
+                    history('/')
+                }} />
+        </div>
+    )
+}
 
-export default AddExpensePage
+export default connect()(AddExpensePage)
