17bc8ccbef045f6097dfb476b32a0515efd5b213
Nnvedward <nnvedward@gmail.com>
2022-01-30 13:10:59 +0100

    Setting up webpack production build

--- src/app.js
+++ src/app.js
@@ -7,18 +7,8 @@ import 'react-dates/lib/css/_datepicker.css'
 import 'react-dates/initialize';
 import AppRouter from './routers/AppRouter'
 import configureStore from './store/configureStore'
-import { addExpense } from './actions/expenses'
-import getVisibleExpenses from './selectors/expenses'
 
 const store = configureStore()
-
-store.dispatch(addExpense({ description: 'Water bill', amount: '200' }))
-store.dispatch(addExpense({ description: 'Gas bill', amount: '100', createdAt: 1000 }))
-store.dispatch(addExpense({ description: 'Rent', amount: '25000' }))
-
-const state = store.getState()
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
-console.log(visibleExpenses)
 
 const jsx = (
     <Provider store={store}>
--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,33 @@
 const path = require('path')
 
-module.exports = {
-    mode: 'development',
-    entry: './src/app.js',
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_modules/
-        }, {
-            test: /\.s?css$/,
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-            ]
-        }]
-    },
-    devtool: 'eval-cheap-module-source-map',
-    devServer: {
-        static: {
-            directory: path.join(__dirname, 'public')
+module.exports = (env) => {
+    return {
+        mode: env.production ? 'production' : 'development',
+        entry: './src/app.js',
+        output: {
+            path: path.join(__dirname, 'public'),
+            filename: 'bundle.js'
         },
-        historyApiFallback: true
+        module: {
+            rules: [{
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/
+            }, {
+                test: /\.s?css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }]
+        },
+        devtool: env.production ? 'source-map' : 'eval-cheap-module-source-map',
+        devServer: {
+            static: {
+                directory: path.join(__dirname, 'public')
+            },
+            historyApiFallback: true
+        }
     }
 }
